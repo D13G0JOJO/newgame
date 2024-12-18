3de950f1590c1bf422b7513d8ccb6b2a0d8d5e8b
D13G0JOJO <19149@vallivue.org>
2024-12-18 12:41:17 -0700

    checking

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -22,7 +22,7 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			sprite.play("jump")
-		
+		# jump reset
 		#direction
 		if Input.is_action_just_pressed("left"):
 			sprite.flip_h = true
