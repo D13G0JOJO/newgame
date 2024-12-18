9c4300f567f5d02e461c8b01d5c80bad188ef7a6
D13G0JOJO <19149@vallivue.org>
2024-12-18 12:52:05 -0700

    player stands up when finish jumping

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -22,6 +22,7 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			sprite.play("jump")
+
 		# jump reset
 		#direction
 		if Input.is_action_just_pressed("left"):
@@ -42,3 +43,7 @@ func die():
 	sprite.play("die")
 	active = false
 	collision_shape.set_deferred("disabled",true)
+
+
+func _on_animation_finished() -> void:
+	sprite.play("idle")
